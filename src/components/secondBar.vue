c196c7b2daf5db3a55987a4f5f6f5af12ffc5b24
zz bct <1668856072@qq.com>
2017-03-30 22:23:16 +0800

    二级导航-secondar

---
+++ src/components/secondBar.vue
@@ -0,0 +1,29 @@
+<!-- 二级顶部 -->
+<style lang="less">
+   .tc-secondbar {
+   	  height: 2.5em;
+   	  font-size: 1em;
+   	  border-top: 0.05em solid #ddd;
+   	  &-span {
+   	  	position: relative;
+   	  	line-height: 2.5em;
+   	  	padding: 0 0.5em;
+   	  	&:after {
+   	  		position: absolute;
+   	  		content: "";
+   	  		top: 0;
+   	  		left: 0;
+   	  		width: 0.1em;
+   	  		height: 100%;
+   	  		background-color: #ce3d3e;
+   	  	}
+   	  }
+   }
+</style>
+<template>
+	<div class="tc-secondbar">
+		<span class="tc-secondbar-span">
+			<slot></slot>
+		</span>
+	</div>
+</template>
--- src/views/recommend.vue
+++ src/views/recommend.vue
@@ -3,17 +3,23 @@
 	<div>
 	   <!-- 走马灯区域 -->
 	   <magic-lamp></magic-lamp>
+	   <!-- 推荐歌单 -->
+	   <second-bar>
+	   	  <router-link to="songList">推荐歌单 ></router-link>
+	   </second-bar>
 	   <!-- 底部音乐播放状态 -->
 	   <footbar></footbar>
 	</div>
 </template>
 <script>
    import footbar from '../components/footBar'
-   import magicLamp from '../components/magicLamp.vue'
+   import magicLamp from '../components/magicLamp'
+   import secondBar from '../components/secondBar'
    export default {
    	 components: {
    	 	footbar,
-   	 	magicLamp
+   	 	magicLamp,
+   	 	secondBar
    	 }
    }
 </script>
