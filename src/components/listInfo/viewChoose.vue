3502c804473bec433e9190d0a85bb95835ab34b9
zz bct <1668856072@qq.com>
2017-03-31 15:58:45 +0800

    播放设置--播放全部、多选-样式

---
+++ src/components/listInfo/viewChoose.vue
@@ -0,0 +1,28 @@
+<!-- 播放设置--播放全部、多选 -->
+<style lang="less">
+	.tc-viewchoose {
+		height: 3em;
+		line-height: 3em;
+		display: flex;
+		justify-content: space-between;
+		&-left {
+			font-size: 0.8em;
+		}
+		&-right {
+			font-size: 0.72em;
+		}
+	}
+</style>
+<template>
+    <div class="tc-viewchoose">
+       <div class="tc-viewchoose-left">
+       	  <span class="icon-solid-play-off"></span>
+    	  <slot></slot>
+    	  <span style="color:gray;font-size:0.9em">(歌曲数)</span>
+       </div>
+       <div class="tc-viewchoose-right">
+       	  <span class="icon-pick-more"></span>
+       	  <span>多选</span>
+       </div>
+    </div>
+</template>
--- src/views/listInfo.vue
+++ src/views/listInfo.vue
@@ -1,12 +1,17 @@
 <!-- 一个歌单下的页面 -->
 <template>
-	<view-control>歌单</view-control>
+    <div>
+       <view-control>歌单</view-control>
+	   <view-choose>播放全部</view-choose>
+    </div>
 </template>
 <script>
    import viewControl from '../components/listInfo/viewControl'
+   import viewChoose from '../components/listInfo/viewChoose.vue'
    export default {
    	 components: {
-   	 	viewControl
+   	 	viewControl,
+   	 	viewChoose
    	 }
    }
 </script>
